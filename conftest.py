7d280222a6e4d4823be664a22e8ddbc9251f6c72
Yana Mazur <mazurihaa@yandex.ru>
2026-02-14 02:33:30 +0300

    Added data cleaning after tests

--- conftest.py
+++ conftest.py
@@ -8,7 +8,7 @@ def api_entities():
     return EntityAPI()
 
 @pytest.fixture
-def created_entity(api_entities):
+def create_entity(api_entities):
     payload = Payload.create_entity()
     entity_id = api_entities.create_entity(payload)
     entity = api_entities.get_entity_by_id(entity_id)
--- tests/test_entity.py
+++ tests/test_entity.py
@@ -8,33 +8,37 @@ from entity.payloads import Payload
 class TestEntities:
 
     @allure.title("Create new entity")
-    def test_create_entity(self,created_entity):
-        assert created_entity.id is not None
+    def test_create_entity(self, api_entities, create_entity):
+        assert create_entity.id is not None
+        api_entities.delete_entity_by_id(create_entity.id)
 
     @allure.title("Get entity")
-    def test_get_entity(self, api_entities, created_entity):
-        entity = api_entities.get_entity_by_id(created_entity.id)
-        assert entity.id == created_entity.id
+    def test_get_entity(self, api_entities, create_entity):
+        entity = api_entities.get_entity_by_id(create_entity.id)
+        assert entity.id == create_entity.id
+        api_entities.delete_entity_by_id(create_entity.id)
 
     @allure.title("Get all entities")
-    def test_get_all_entities(self, api_entities, created_entity):
+    def test_get_all_entities(self, api_entities, create_entity):
         entities = api_entities.get_all_entity()
         assert entities is not None
+        api_entities.delete_entity_by_id(create_entity.id)
 
     @allure.title("Update entity")
-    def test_update_entity(self, api_entities, created_entity):
+    def test_update_entity(self, api_entities, create_entity):
         update_payload = Payload.update_entity()
 
         # Метод вернёт обновлённую сущность после GET
         updated_entity = api_entities.patch_entity_by_id(
-            created_entity.id,
+            create_entity.id,
             payload=update_payload
         )
         # Проверяем, что поле обновилось
         assert updated_entity.title == update_payload["title"]
+        api_entities.delete_entity_by_id(create_entity.id)
 
     @allure.title("Delete entity")
-    def test_delete_entity(self, api_entities, created_entity):
-        delete_entity = api_entities.delete_entity_by_id(created_entity.id)
+    def test_delete_entity(self, api_entities, create_entity):
+        delete_entity = api_entities.delete_entity_by_id(create_entity.id)
         assert delete_entity == 204
 
